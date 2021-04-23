e74c56350f1ab549e4d1d9ab2fe52ec97e0ef58e
djahern-max <danielaherniv@gmail.com>
2021-04-22 23:10:37 -0400

    11.  Showing the newly posted comment

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -1,7 +1,12 @@
 <?php
 require_once("../includes/config.php");
+require_once("../includes/classes/User.php");
+require_once("../includes/classes/Comment.php");
 
 if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])) {
+    
+    $userLoggedInObj = new User($con, $_SESSION["userLoggedIn"]);
+
     $query = $con->prepare("INSERT INTO comments(postedBy, videoId, responseTo, body)
                             VALUES(:postedBy, :videoId, :responseTo, :body)");
     $query->bindParam(":postedBy", $postedBy);
@@ -16,9 +21,8 @@ if(isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['vi
 
     $query->execute();
 
-    // $newComment = new Comment($con, $con->lastInsertId(), $userLoggedInObj, $videoId);
-    // echo $newComment->create();
-
+    $newComment = new Comment($con, $con->lastInsertId(), $userLoggedInObj, $videoId);
+    echo $newComment->create();
 }
 else {
     echo "One or more parameters are not passed into subscribe.php the file";
