f2f2c776ae636ac16660186364c146239c8b509b
djahern-max <danielaherniv@gmail.com>
2021-04-25 23:21:44 -0400

    133. Like commeent ajax response

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 ob_start(); //Turns on output buffering 
 session_start();
 
-date_default_timezone_set("Europe/London");
+date_default_timezone_set("America/New_York");
 
 try {
     $con = new PDO("mysql:dbname=flipstock;host=localhost:3307", "root", "");
