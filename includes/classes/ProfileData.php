2d028dc0675d28b41b4edbe03f4126de30d64d89
djahern-max <danielaherniv@gmail.com>
2021-05-04 03:56:35 -0400

    Finished the profile page

--- includes/classes/ProfileData.php
+++ includes/classes/ProfileData.php
@@ -9,6 +9,10 @@ class ProfileData {
         $this->profileUserObj = new User($con, $profileUsername);
     }
 
+    public function getProfileUserObj() {
+        return $this->profileUserObj;
+    }
+
     public function getProfileUsername() {
         return $this->profileUserObj->getUsername();
     }
@@ -30,12 +34,50 @@ class ProfileData {
         return $this->profileUserObj->getName();
     }
 
-    // public function getProfilePic() {
-    //     return $this->profileUserObj->getProfilePic();
-    // }
+    public function getProfilePic() {
+        return $this->profileUserObj->getProfilePic();
+    }
 
-    // public function getSubscriberCount() {
-    //     return $this->profileUserObj->getSubscriberCount();
-    // }
+    public function getSubscriberCount() {
+        return $this->profileUserObj->getSubscriberCount();
+    }
+
+    public function getUsersVideos() {
+        $query = $this->con->prepare("SELECT * FROM videos WHERE uploadedBy=:uploadedBy ORDER BY uploadDate DESC");
+        $query->bindParam(":uploadedBy", $username);
+        $username = $this->getProfileUsername();
+        $query->execute();
+
+        $videos = array();
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $videos[] = new Video($this->con, $row, $this->profileUserObj->getUsername());
+        }
+        return $videos;
+    }
+
+    public function getAllUserDetails() {
+        return array(
+            "Name" => $this->getProfileUserFullName(),
+            "Username" => $this->getProfileUsername(),
+            "Subscribers" => $this->getSubscriberCount(),
+            "Total views" => $this->getTotalViews(),
+            "Sign up date" => $this->getSignUpDate()
+        );
+    }
+
+    private function getTotalViews() {
+        $query = $this->con->prepare("SELECT sum(views) FROM videos WHERE uploadedBy=:uploadedBy");
+        $query->bindParam(":uploadedBy", $username);
+        $username = $this->getProfileUsername();
+        $query->execute();
+
+        return $query->fetchColumn();
+    }
+
+    private function getSignUpDate() {
+        $date = $this->profileUserObj->getSignUpDate();
+        return date("F jS, Y", strtotime($date));
+    }
+
 }
 ?>
