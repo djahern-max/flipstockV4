26e1a54e7630408290f670590fd939cce5e86bd8
djahern-max <danielaherniv@gmail.com>
2021-05-15 22:38:27 -0400

    changed con variable from conn to con

--- includes/config.php
+++ includes/config.php
@@ -15,7 +15,7 @@ try {
     $active_group = 'default';
     $query_builder = TRUE;
     // Connect to DB
-    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 }
 catch (PDOException $e) {
