ee77e6d28e4b1172c9dd1c5b2e2369548a705db7
djahern-max <danielaherniv@gmail.com>
2021-05-15 22:23:42 -0400

    updated config.php to connect to database

--- includes/config.php
+++ includes/config.php
@@ -5,8 +5,18 @@ session_start();
 date_default_timezone_set("America/New_York");
 
 try {
-    $con = new PDO("mysql:dbname=flipstock;host=localhost:3307", "root", "");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+ 
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 }
 catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
