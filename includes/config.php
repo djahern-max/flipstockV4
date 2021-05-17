f611698a3e3f149627e36e62e497d13abace765e
djahern-max <danielaherniv@gmail.com>
2021-05-16 22:19:16 -0400

    update config

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,3 @@
-
 <?php
 ob_start(); //Turns on output buffering 
 session_start();
@@ -8,17 +7,19 @@ date_default_timezone_set("America/New_York");
 try {
  
     //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    // $cleardb_server = $cleardb_url["host"];
+    // $cleardb_username = $cleardb_url["user"];
+    // $cleardb_password = $cleardb_url["pass"];
+    // $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $con = new PDO("mysql:dbname=heroku_e7268af48fdbee0;host=us-cdbr-east-03.cleardb.com", "bf20c28b62e85b", "23b244e6");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
 }
 catch (PDOException $e) {
-    echo "Connection failed now: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
